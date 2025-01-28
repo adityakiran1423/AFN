2ab3e98d5a1f7db051b0d3b02321b32b5abc27f1
adityakiran1423 <adi.kiran@gmail.com>
2025-01-29 00:38:42 +0530

    json to dict working

--- data_populator.py
+++ data_populator.py
@@ -9,23 +9,43 @@ load_dotenv()
 
 OMDB_API_KEY = os.getenv("OMDB_API_KEY")
 
+movie_name = "At Eternity's Gate"
+details = []
+url = f"https://www.omdbapi.com/?t={movie_name}&plot=full&apikey={OMDB_API_KEY}&"
+response = requests.get(url)
+response.json
+obj = json.loads(response.text)
+# output = json.dumps(obj, indent=4)
+# print(output)
+# print(type(output))
+t = json.dumps(obj, indent = 4)
+output = json.loads(t)
+# print(output)
+for i in output:
+    print(i)
+# for data in output:
+#     t = output[1]
+#     print(t)
+    # details.append(t)
 
-def extract_movie_name_from_csv():
-    movie_name = ""
-    with open('Movies.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        # print("here")
-        for row in reader:
-            movie_name = row[0]
-            url = url=f"https://www.omdbapi.com/?t={movie_name}&plot=full&apikey={OMDB_API_KEY}&"
-            response = requests.get(url)
-            response.json
-            obj = json.loads(response.text)
-            print(json.dumps(obj, indent=4))
-            print("\n-----------------------\n")
-            # print(obj["Plot"])
+# print("Printing details")
+# print(details)
 
-    return movie_name
+# def extract_movie_name_from_csv():
+#     movie_name = ""
+#     with open('Movies.csv', newline='') as csvfile:
+#         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+#         for row in reader:
+#             movie_name = row[0]
+#             url = f"https://www.omdbapi.com/?t={movie_name}&plot=full&apikey={OMDB_API_KEY}&"
+#             response = requests.get(url)
+#             response.json
+#             obj = json.loads(response.text)
+#             print(json.dumps(obj, indent=4))
+#             print("\n-----------------------\n")
 
 
-extract_movie_name_from_csv()
+#     return movie_name
+
+
+# extract_movie_name_from_csv()
