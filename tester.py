8146e86acedcf2336b00e2083fafdac2444aeaf6
adityakiran1423 <adi.kiran@gmail.com>
2025-02-08 16:05:24 +0530

    adding .txt for names and seperate file for testing

---
+++ tester.py
@@ -0,0 +1,38 @@
+import os
+import requests
+# import csv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OMDB_API_KEY = os.getenv("OMDB_API_KEY")
+
+movie_name = "Petonjee"
+details = []
+url = f"https://www.omdbapi.com/?t={movie_name}&plot=full&apikey={OMDB_API_KEY}&"
+response = requests.get(url)
+json_response = response.json()
+
+
+attribute_to_remove_list = [
+    'Rated',
+    'Writer',
+    'Language',
+    'Country',
+    'Awards',
+    'Metascore',
+    'imdbID',
+    'DVD',
+    'BoxOffice',
+    'Production',
+    'Website',
+]
+attributes = [key for key in json_response]
+
+for i in range(len(attribute_to_remove_list)):
+    if attributes[i] in attribute_to_remove_list:
+        json_response.pop(response[i])
+
+for key, value in json_response.items():
+    print(key, " -> ", value)
