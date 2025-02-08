0976aba856b695201c99b453b4d2c89c7886efc4
adityakiran1423 <adi.kiran@gmail.com>
2025-02-08 16:04:57 +0530

    fix : KeyError fixed for movies whose data not found

--- data_populator.py
+++ data_populator.py
@@ -1,7 +1,7 @@
-import os    
+import os
 import csv
 import requests
-import json
+# import json
 
 from dotenv import load_dotenv
 
@@ -9,43 +9,50 @@ load_dotenv()
 
 OMDB_API_KEY = os.getenv("OMDB_API_KEY")
 
-movie_name = "At Eternity's Gate"
-details = []
-url = f"https://www.omdbapi.com/?t={movie_name}&plot=full&apikey={OMDB_API_KEY}&"
-response = requests.get(url)
-response.json
-obj = json.loads(response.text)
-# output = json.dumps(obj, indent=4)
-# print(output)
-# print(type(output))
-t = json.dumps(obj, indent = 4)
-output = json.loads(t)
-# print(output)
-for i in output:
-    print(i)
-# for data in output:
-#     t = output[1]
-#     print(t)
-    # details.append(t)
+def extract_movie_name_from_csv():
+    movie_name = ""
+    with open("Movies.csv", newline="") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",", quotechar="|")
+        for row in reader:
+            movie_name = row[0]
+            url = f"https://www.omdbapi.com/?t={movie_name}&plot=full&apikey={OMDB_API_KEY}&"
+            response = requests.get(url)
+            json_response = response.json()
+            pruned_ouput = prune_api_response(json_response)
 
-# print("Printing details")
-# print(details)
-
-# def extract_movie_name_from_csv():
-#     movie_name = ""
-#     with open('Movies.csv', newline='') as csvfile:
-#         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-#         for row in reader:
-#             movie_name = row[0]
-#             url = f"https://www.omdbapi.com/?t={movie_name}&plot=full&apikey={OMDB_API_KEY}&"
-#             response = requests.get(url)
-#             response.json
-#             obj = json.loads(response.text)
-#             print(json.dumps(obj, indent=4))
-#             print("\n-----------------------\n")
+    return pruned_ouput
 
 
-#     return movie_name
+def prune_api_response(response):
+    if response['Response'] == 'False':
+        # think of something better to return 
+        return "movie not found"
+    
+    attribute_to_remove_list = [
+        'Rated',
+        'Writer',
+        'Language',
+        'Country',
+        'Awards',
+        'Metascore',
+        'imdbID',
+        'DVD',
+        'BoxOffice',
+        'Production',
+        'Website',
+    ]
+    print(len(response))
+
+    print(response)
+    attributes = [key for key in response]
+    for i in range(len(response)):
+        if attributes[i] in attribute_to_remove_list:
+            response.pop(attributes[i])
+
+    for key, value in response.items():
+        print(key, " -> ", value)
+
+    return response
 
 
-# extract_movie_name_from_csv()
+extract_movie_name_from_csv()
